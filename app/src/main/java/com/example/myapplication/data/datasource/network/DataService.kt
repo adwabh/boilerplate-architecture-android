069c7f0cd6ce893cf9f73854b6e28ab4f3b311a7
Adwait Abhyankar <adwait.abhyankar@go-jek.com>
2022-01-06 11:42:22 +0530

    [Adwait] movies sample initial commit

--- app/src/main/java/com/example/myapplication/data/datasource/network/DataService.kt
+++ app/src/main/java/com/example/myapplication/data/datasource/network/DataService.kt
@@ -1,4 +1,8 @@
 package com.example.myapplication.data.datasource.network
 
+import com.example.myapplication.data.models.Movies
+import retrofit2.Response
+
 interface DataService {
+    suspend fun getMovies() : Response<Movies>
 }
