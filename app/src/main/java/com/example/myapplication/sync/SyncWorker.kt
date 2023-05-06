4ae6b7a90c14d502207dec4b820e45769545ea40
Adwait Abhyankar <avabhyankar22@gmail.com>
2023-05-06 15:45:48 +0530

    [NOTE-001][Adwait] sync worker WIP

--- app/src/main/java/com/example/myapplication/sync/SyncWorker.kt
+++ app/src/main/java/com/example/myapplication/sync/SyncWorker.kt
@@ -2,7 +2,10 @@ package com.example.myapplication.sync
 
 import android.content.Context
 import androidx.hilt.work.HiltWorker
+import androidx.work.Constraints
 import androidx.work.CoroutineWorker
+import androidx.work.Data
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.OutOfQuotaPolicy
 import androidx.work.WorkerParameters
@@ -11,6 +14,7 @@ import com.myapplication.example.core.data.notes.model.User
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 
+const val WORKER_CLASS_NAME: String = "NotesSyncWorker"
 
 @HiltWorker
 class SyncWorker @AssistedInject constructor(
@@ -18,21 +22,27 @@ class SyncWorker @AssistedInject constructor(
     @Assisted params: WorkerParameters,
     private val repository: NotesRepository,
     val userPref: UserPreferences
-) : CoroutineWorker(appContext,
-    params) {
+) : CoroutineWorker(
+    appContext, params
+) {
     override suspend fun doWork(): Result {
-        return if(repository.syncNotes(userPref.getUser())) Result.success() else Result.failure()
+        return if (repository.syncNotes(userPref.getUser())) Result.success() else Result.failure()
     }
 
     companion object {
+
+        val SyncConstraints
+            get() = Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
+
+
         /**
          * Expedited one time work to sync data on app startup
          */
-        fun startUpSyncWork() = OneTimeWorkRequestBuilder<DelegatingWorker>()
-            .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
-            .setConstraints(SyncConstraints)
-            .setInputData(SyncWorker::class.delegatedData())
-            .build()
+        fun startUpSyncWork() =
+            OneTimeWorkRequestBuilder<SyncWorker>().setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
+                .setConstraints(SyncConstraints).setInputData(SyncWorker.delegatedData()).build()
+
+        private fun delegatedData(): Data = Data.Builder().putString(WORKER_CLASS_NAME, SyncWorker::class.qualifiedName).build()
     }
 
 }
