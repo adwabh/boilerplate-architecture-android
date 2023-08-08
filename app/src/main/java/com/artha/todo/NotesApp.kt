8014a79ff374da7e55a4b7dab6680f6de67de040
Adwait Abhyankar <avabhyankar22@gmail.com>
2023-08-09 00:41:47 +0530

    [NOTE-003A][Adwait] sync init added

--- app/src/main/java/com/artha/todo/NotesApp.kt
+++ app/src/main/java/com/artha/todo/NotesApp.kt
@@ -1,8 +1,14 @@
 package com.artha.todo
 
 import android.app.Application
+import com.artha.todo.work.SyncImpl
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class NotesApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        SyncImpl.initialize(this)
+    }
 }
--- app/src/main/java/com/artha/todo/work/SyncWorker.kt
+++ app/src/main/java/com/artha/todo/work/SyncWorker.kt
@@ -1,15 +1,24 @@
 package com.artha.todo.work
 
 import android.content.Context
+import androidx.startup.AppInitializer
+import androidx.startup.Initializer
+import androidx.work.Constraints
+import androidx.work.ExistingWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.OutOfQuotaPolicy
 import androidx.work.WorkManager
+import androidx.work.WorkManagerInitializer
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.artha.todo.data.UserPreference
 import com.artha.todo.data.repo.NotesRepository
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
+
+const val SyncWorkName = "NOTES_SYNC_WORK"
 
 class SyncWorker @Inject constructor(
     context: Context,
@@ -24,15 +33,41 @@ class SyncWorker @Inject constructor(
         return if(result) Result.success() else Result.failure()
     }
 }
-class SyncImpl @Inject constructor(@ApplicationContext val context: Context) : Sync {
-    override fun sync() {
-        val request = OneTimeWorkRequestBuilder<SyncWorker>()
-            .build()
-        WorkManager.getInstance(context)
-            .enqueue(request)
+object SyncImpl : Sync {
+
+    override fun initialize(context: Context) {
+        AppInitializer.getInstance(context)
+            .initializeComponent(SyncInitializer::class.java)
     }
 
+    private val SyncConstraints = Constraints.Builder()
+        .setRequiredNetworkType(NetworkType.CONNECTED)
+        .build()
+
+    fun sync(): OneTimeWorkRequest {
+        return OneTimeWorkRequestBuilder<SyncWorker>()
+            .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
+            .setConstraints(SyncConstraints)
+            .build()
+    }
+}
+
+object SyncInitializer : Initializer<SyncImpl> {
+
+    override fun create(context: Context): SyncImpl {
+        WorkManager.getInstance(context).apply {
+            enqueueUniqueWork(
+                SyncWorkName,
+                ExistingWorkPolicy.KEEP,
+                SyncImpl.sync()
+                )
+        }
+        return SyncImpl
+    }
+
+    override fun dependencies(): List<Class<out Initializer<*>>> = listOf(
+        WorkManagerInitializer::class.java)
 }
 interface Sync {
-    abstract fun sync()
+    abstract fun initialize(context: Context)
 }
